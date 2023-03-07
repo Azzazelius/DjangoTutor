f9acf05759660815e530be3646134bae5e90fb58
Aleksandr Zhevnerov <azzazelius@gmail.com>
2023-03-07 22:18:25 +0300

    added new pages and css

--- project_dir/tutor_project/tutor_project/settings.py
+++ project_dir/tutor_project/tutor_project/settings.py
@@ -38,8 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'django.contrib.sites',
-    'django.contrib.flatpages'
+    'django.contrib.flatpages',
+    'fpages'
 ]
 
 MIDDLEWARE = [
@@ -128,3 +130,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFFILES_DIRS = [BASE_DIR / 'static']
